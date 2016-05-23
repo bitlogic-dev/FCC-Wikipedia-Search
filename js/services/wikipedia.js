0584fa5a240ad0c1ca08799b1501547090c67d4a
Haig Hagopian <neofusionzero@users.noreply.github.com>
2016-05-23 16:59:40 -0700

    tidy up

--- js/services/wikipedia.js
+++ js/services/wikipedia.js
@@ -1,9 +1,9 @@
 app.factory ('wikipedia', ['$http', function($http){
-return $http.get('https://en.wikipedia.org/w/api.php')
-.success(function(data){
-return data;
-})
-.error(function(err){
-return err;
-});
+  return $http.get('https://en.wikipedia.org/w/api.php')
+  .success(function(data){
+    return data;
+  })
+  .error(function(err){
+    return err;
+  });
 }]);
